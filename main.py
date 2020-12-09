743f289de35c2bcb95b005575ac748f0ced18542
Onkarpawar369 <56354664+Onkarpawar369@users.noreply.github.com>
2020-12-09 20:26:37 +0530

    full running code with features like playing video on youtube,get current time, find person's info,to find jokes

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import speech_recognition as sr
 import pyaudio
+import pywhatkit
 import pyttsx3
-
+import datetime
+import wikipedia
+import pyjokes
+listener=sr.Recognizer()
 engine=pyttsx3.init()
 voices=engine.getProperty('voices')
 voices=engine.setProperty('voice',voices[1].id)
@@ -10,15 +14,35 @@ def talk(text):
     engine.say(text)
     engine.runAndWait()
 
-listener=sr.Recognizer()
-try:
-    with sr.Microphone() as source:
-        print('listening.....')
-        voice=listener.listen(source)
-        command=listener.recognize_google(voice)
-        command=command.lower()
-        if 'alexa' in command:
-            talk(command)
-            print(command)
-except:
-    pass
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening.....')
+            voice=listener.listen(source)
+            command=listener.recognize_google(voice)
+            command=command.lower()
+            if 'alexa' in command:
+                command=command.replace('alexa','')
+                print(command)
+    except:
+        pass
+    return command
+
+def run_alexa():
+    statement=take_command()
+    if 'play' in statement:
+        song=statement.replace('play','')
+        talk('playing'+song)
+        pywhatkit.playonyt(song)
+    if 'time' in statement:
+        time=datetime.datetime.now().strftime('%I:%M %p')
+        print(time)
+        talk('current time is'+time)
+    if 'who is' in statement:
+        person=statement.replace('who is','')
+        info=wikipedia.summary(person,1)
+        talk(info)
+    if 'joke' in statement:
+        jokes=pyjokes.get_joke()
+        talk(jokes)
+run_alexa()
