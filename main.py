2e5302cdeecaeede721fa48d5b8ec19c4564d6fe
Onkarpawar369 <56354664+Onkarpawar369@users.noreply.github.com>
2020-12-09 17:46:52 +0530

    with speech recognittion and text to speech featuress added

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-import dlib
 import speech_recognition as sr
+import pyaudio
+import pyttsx3
+
+engine=pyttsx3.init()
+voices=engine.getProperty('voices')
+voices=engine.setProperty('voice',voices[1].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+listener=sr.Recognizer()
+try:
+    with sr.Microphone() as source:
+        print('listening.....')
+        voice=listener.listen(source)
+        command=listener.recognize_google(voice)
+        command=command.lower()
+        if 'alexa' in command:
+            talk(command)
+            print(command)
+except:
+    pass
